530d1ea13bcf19fcde1783112525402221aa314c
Petr Gregor <petr.gregor@email.cz>
2025-01-30 20:37:03 +0100

    Mongo - Exercise 1 solution.

--- p03_mongo/p06_Exercise_1_medical_data.py
+++ p03_mongo/p06_Exercise_1_medical_data.py
@@ -4,11 +4,35 @@ Use data in medical-data.json to create a new collection: medicaldata
 - Find patient with patient_id equal 74, print his full name
 - Find a procedure performed on 2019-05-24T01:52:37.000Z and update its procedure code to 0F1F4ZC
 """
+import json
 
-# TODO: Use data in medical-data.json to create a new collection: medicaldata
+from connect_mongo import db_test_Z4073
 
-# TODO: Find all rows with procedure_code equal 0F1F4ZC
 
-# TODO: Find patient with patient_id equal 74, print his full name
+# Use data in medical-data.json to create a new collection: medicaldata
+medicaldata = db_test_Z4073["medicaldata"]
 
-# TODO: Find a procedure performed on 2019-05-24T01:52:37.000Z and update its procedure code to 0F1F4ZC
+"""
+with open('files/medical-data.json') as f:
+    data = json.load(f)
+    response = medicaldata.insert_many(data)
+    print(f"To collection 'medicaldata' were inserted {len(response.inserted_ids)} documents.")
+"""
+
+print("Find all rows with procedure_code equal 0F1F4ZC:")
+for procedure in medicaldata.find({"procedure_code": "0F1F4ZC"}):
+    print(procedure)
+
+print("Find patient with patient_id equal 74, print his full name:")
+patient = medicaldata.find_one({"patient_id": 74})
+print(f"{patient['first_name']} {patient['last_name']}")
+
+print("Find a procedure performed on 2019-05-24T01:52:37.000Z and update its procedure code to 0F1F4ZC:")
+for visit in medicaldata.find({"visit_date.date": "2019-05-24T01:52:37.000Z"}):
+    print(visit)
+
+print('-'*40)
+response = medicaldata.update_many({"visit_date.date": "2019-05-24T01:52:37.000Z"},
+                                   {"$set": {"procedure_code": "0F1F4ZC"}})
+for visit in medicaldata.find({"visit_date.date": "2019-05-24T01:52:37.000Z"}):
+    print(visit)
